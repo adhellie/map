d9dae5ebb9b30ecfa968875119c8872225d12f1c
Adelie <mediadelie@gmail.com>
2016-07-19 17:15:58 +0200

    mise en place des pointers

--- js/modules/map.js
+++ js/modules/map.js
@@ -1,26 +1,25 @@
 var Script = module.exports = function(){
 
   this.bind = function(){
-
-  var map = document.querySelector('.map') ? document.querySelector('.map') : false ;
-  if (map) {
-    createMap(map);
+  var mymap;
+  if (this.els.map) {
+    createMap(this.els.map);
+    var checkboxes = [].slice.call(document.querySelector('.mapCtn').querySelectorAll('[type="checkbox"]'));
+    console.log(checkboxes);
+    checkboxes.forEach(function(input){
+      input.addEventListener('click',createPointer);
+    })
+    
   } 
   else {
     alert("Aucune carte n'est disponible");
   }
 
-    // var marker = L.marker([48.77, -3.6]).addTo(mymap);
-    // marker.bindPopup("<b>Île Millau</b>").openPopup();
-
-    // var marker2 = L.marker([48.765, -3.58]).addTo(mymap);
-    // marker2.bindPopup("<b>Plage de Tresmeur</b>").openPopup();
-
   };
 
   this.dom = function(){
     this.els = {
-
+      map : document.querySelector('.map') ? document.querySelector('.map') : false
     };
 
   };
@@ -39,7 +38,7 @@ var Script = module.exports = function(){
 
 function createMap(el){
   console.log(el);
-  var mymap = L.map(el.getAttribute('id')).setView([el.dataset.x, el.dataset.y], 13);
+  mymap = L.map(el.getAttribute('id')).setView([el.dataset.x, el.dataset.y], 13);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
     maxZoom: 20,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -47,5 +46,16 @@ function createMap(el){
       'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.streets'
   }).addTo(mymap);
+}
+
+function createPointer(){
+  var marker;
+  if(this.checked===true){
+    marker =L.marker([this.dataset.x, this.dataset.y]).addTo(mymap).bindPopup("<b>" + this.dataset.title + "</b>").openPopup();
+  } else {
+    //should remove the marker
+  }
+
+  
 }
 
